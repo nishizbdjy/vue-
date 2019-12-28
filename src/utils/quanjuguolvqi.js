73ede7dac497641ac45cf724659b510d9efe2530
lbw <2659154921@qq.com>
2019-12-28 18:50:04 +0800

    完成全局过滤器

---
+++ src/utils/quanjuguolvqi.js
@@ -0,0 +1,11 @@
+//这是全局过滤器
+import vue from 'vue'//
+
+export const quanjuglq = vue.filter('glq', (tiem, p) => {
+    p = p || '-'
+    let nian = tiem.getFullYaer()//年
+    let yue = tiem.getMonth() + 1//月
+    let ri = tiem.getDate()//日
+    let riqi = `${nian}${p}${yue}${p}${ri}`
+     return riqi
+})
