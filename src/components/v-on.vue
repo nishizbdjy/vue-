cbe9069402f3d148e205c849dad9ba53c2359af2
lbw <2659154921@qq.com>
2019-12-28 17:15:00 +0800

    完成v-on

---
+++ src/components/v-on.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <a href="#" v-on:click="w">你点我啊</a>
+    <br />
+    <a href="http://www.baidu.com" @click.prevent="w">事件修饰符</a>
+    <input type="text" @keydown.13="w('fdrfd',$event)" />
+    <br>
+    <button @[msg]="w">双击</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "dblclick"
+    };
+  },
+  methods: {
+    w(gh, d) {
+      console.log(gh);
+      console.log(d);
+      console.log("我被点了");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 // import App from './App.vue'
-import App from '@/components/v-model.vue'
+import App from '@/components/v-on.vue'
 
 
 Vue.config.productionTip = false
